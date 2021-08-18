5e4836ca0301d5ec1e0bf6efe60bb9413622e8e6
Andrzej Zabost <andrzej.zabost@brightinventions.pl>
2021-08-18 14:19:40 +0200

    Migrate to Kotlin 1.5.21 and kotlinx.serialization 1.2.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
-    const val kotlin = "1.4.31"
+    const val kotlin = "1.5.21"
     const val junit = "5.6.2"
     const val shouldko = "0.2.2"
-    const val serialization = "1.1.0"
+    const val serialization = "1.2.2"
 }
