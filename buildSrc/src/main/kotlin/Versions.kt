ec17f301af0322fa935261c6875b6bcc7d6d9b45
Andrzej Zabost <andrzej.zabost@brightinventions.pl>
2021-12-24 18:00:41 +0100

    Update Kotlin to 1.6.10 and serialization to 1.3.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
-    const val kotlin = "1.5.32"
+    const val kotlin = "1.6.10"
     const val junit = "5.6.2"
     const val shouldko = "0.2.2"
-    const val serialization = "1.3.0"
+    const val serialization = "1.3.2"
 }
